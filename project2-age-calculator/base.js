aaca37e71932cb125a6d1bae8c91c3adc792ea7e
samson ogbonna <samsonogbonna95@gmail.com>
2023-12-11 10:12:27 +0100

    innitial commit

---
+++ project2-age-calculator/base.js
@@ -0,0 +1,4 @@
+// initialize the input value
+const dateinput = document.getElementById("date");
+const displayResult = document.getElementById("result-display");
+const calculateBtn = document.getElementById("calc-btn");
