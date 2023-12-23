fa97ed9dde9bdb613966272e54edc68d6affc3b5
samson ogbonna <samsonogbonna95@gmail.com>
2023-12-23 06:38:28 +0100

    updated commit

--- project2-age-calculator/script.js
+++ project2-age-calculator/script.js
@@ -1,41 +1,47 @@
 // initialize the value
 
-const formInput = document.getElementById("calculateAge");
-const dateInput = document.getElementById("date");
-const calculateBtn = document.getElementById("calc-btn");
-const displayResult = document.getElementById("result-display");
-
-let dateValid = true;
-
-dateInput.addEventListener("keyup", function (event) {
-  let value = event.target.valueAsNumber;
-  if (value == isNaN || !value) {
-    alert("Please Enter Valid Format");
-  } else {
-    return dateInput.value;
-  }
-});
-
-function clearResult() {
-  dateResult.innerHTML = "";
-}
-
-const isValidDate = function validDate(date) {
-  return (
-    Object.prototype.toString.call(date) === ["object Date"] && !isNaN(date)
-  );
-};
-
 function calculateAge() {
-  const ageInDays = today - birthDate;
-  const ageInMonth = Math.floor(ageInDays / 30.43675);
-  const ageInYears = Math.floor(ageInDays / 365.25);
+  const dateInput = document.getElementById("date");
+  const displayResult = document.getElementById("result-display");
 
-  // compute the total
-  let total = (ageInDays * ageInMonth * ageInYears) / 365.25;
+  let dateValid = true;
 
-  // display the result
-  displayResult.innerHTML = "You are" + total + "years old";
+  dateInput.addEventListener("change", function (event) {
+    let value = event.target.value;
+    console.log(value);
+    if (!value) {
+      alert("Please Enter Valid Format");
+    } else {
+      return;
+    }
+  });
+
+  var dob = new Date(dateInput.value);
+  if (dateInput.value == "" || dateInput.value == null) {
+    alert("Enter a valid date");
+    return;
+  }
+
+  if (dob >= new Date()) {
+    alert("Can Not Display Future Date");
+    return;
+  }
+
+  // calculate month difference from current date in time
+  var month_diff = Date.now() - dob.getTime();
+
+  //convert the calculated difference in date format
+  var age_dt = new Date(month_diff);
+
+  //extract year from date
+  var year = age_dt.getUTCFullYear();
+
+  //now calculate the age of the user
+  var age = Math.abs(year - 1970);
+
+  //display the calculated age
+
+  let result = document.getElementById("result-display");
+  result.innerHTML = "you are " + age + " " + "years old";
+  return;
 }
-
-document.getElementById("calc-btn").addEventListener("click", calculateAge);
