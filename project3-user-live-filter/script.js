703eeaa591fff420550a0e6909c49837abfa9a4f
samson ogbonna <samsonogbonna95@gmail.com>
2024-01-06 14:32:43 +0100

    refactor loadUser to use async-await

--- project3-user-live-filter/script.js
+++ project3-user-live-filter/script.js
@@ -3,51 +3,51 @@ loadUsers();
 document.getElementById("clickButton").addEventListener("click", (e) => {
   var searchValue = document.getElementById("search").value;
   loadUsers(searchValue);
-  console.log(searchValue);
+  console.log("search value", searchValue);
 });
 
-function loadUsers(searchValue = "") {
+async function loadUsers(searchValue = "") {
   var users = [];
 
   var mainElement = document.getElementById("mainSection");
 
-  fetch("./user.json")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log("users", data);
+  const response = await fetch("./user.json");
+  if (!response.ok) {
+    const message = `An error occurred : ${response.status}`;
+    throw new Error(message);
+  }
+  const data = await response.json();
+  console.log("users", data);
 
-      if (searchValue) {
-        users = data.filter(function (user) {
-          return (
-            user.firstName.toLowerCase().includes(searchValue.toLowerCase()) ||
-            user.lastName.toLowerCase().includes(searchValue.toLowerCase()) ||
-            user.city.toLowerCase().includes(searchValue.toLowerCase()) ||
-            user.country.toLowerCase().includes(searchValue.toLowerCase())
-          );
-        });
-        mainElement.innerHTML = "";
-        console.log(users);
-      } else {
-        users = data;
-      }
-
-      for (let i = 0; i < users.length; i++) {
-        var userDivString = `
-        <figure class="figure">
-          <img src="images/team.jpg" alt="football team">
-          <article>${users[i].firstName} ${users[i].lastName}</article>
-          <figcaption>${users[i].city} ${users[i].country}</figcaption>
-        </figure>
-  `;
-
-        const parser = new DOMParser();
-        var userDiv = parser.parseFromString(userDivString, "text/html").body
-          .firstChild;
-        mainElement.append(userDiv);
-      }
+  if (searchValue) {
+    users = data.filter(function (user) {
+      return (
+        user.firstName.toLowerCase().includes(searchValue.toLowerCase()) ||
+        user.lastName.toLowerCase().includes(searchValue.toLowerCase()) ||
+        user.city.toLowerCase().includes(searchValue.toLowerCase()) ||
+        user.country.toLowerCase().includes(searchValue.toLowerCase())
+      );
     });
+    mainElement.innerHTML = "";
+    console.log(users);
+  } else {
+    users = data;
+  }
+
+  for (let i = 0; i < users.length; i++) {
+    var userDivString = `
+    <figure class="figure">
+      <img src="images/team.jpg" alt="football team">
+      <article>${users[i].firstName} ${users[i].lastName}</article>
+      <figcaption>${users[i].city} ${users[i].country}</figcaption>
+    </figure>
+`;
+
+    const parser = new DOMParser();
+    var userDiv = parser.parseFromString(userDivString, "text/html").body
+      .firstChild;
+    mainElement.append(userDiv);
+  }
 }
 
 //display users on html
