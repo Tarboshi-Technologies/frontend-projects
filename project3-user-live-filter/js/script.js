751f19b67f87d3abe964297a1016233f97fdc249
samson ogbonna <samsonogbonna95@gmail.com>
2024-02-02 06:38:41 +0100

    created new files

--- project3-user-live-filter/js/script.js
+++ project3-user-live-filter/js/script.js
@@ -1,6 +1,6 @@
 loadUsers();
 
-$("#clickButton").click(function (e) {
+$("#clickButton").on("click", function (e) {
   var searchValue = $("#search").val();
   loadUsers(searchValue);
   console.log("search value", searchValue);
@@ -11,45 +11,39 @@ async function loadUsers(searchValue = "") {
 
   var mainElement = $("#mainSection");
 
-  const response = await fetch("js/user.json");
-  if (!response.ok) {
-    const message = `An error occurred : ${response.status}`;
-    throw new Error(message);
-  }
-  const data = await response.json();
-  console.log("users", data);
+  $.get("js/user.json", function (data, status) {
+    console.log(data, status);
 
-  if (searchValue) {
-    users = data.filter(function (user) {
-      return (
-        user.firstName.toLowerCase().includes(searchValue.toLowerCase()) ||
-        user.lastName.toLowerCase().includes(searchValue.toLowerCase()) ||
-        user.city.toLowerCase().includes(searchValue.toLowerCase()) ||
-        user.country.toLowerCase().includes(searchValue.toLowerCase())
-      );
-    });
-    mainElement.html("");
-    console.log(users);
-  } else {
-    users = data;
-  }
+    if (searchValue) {
+      users = data.filter(function (user) {
+        return (
+          user.firstName.toLowerCase().includes(searchValue.toLowerCase()) ||
+          user.lastName.toLowerCase().includes(searchValue.toLowerCase()) ||
+          user.city.toLowerCase().includes(searchValue.toLowerCase()) ||
+          user.country.toLowerCase().includes(searchValue.toLowerCase())
+        );
+      });
+      mainElement.html("");
+      // console.log(users);
+    } else {
+      users = data;
+    }
 
-  for (let i = 0; i < users.length; i++) {
-    var userDivString = `
-    <figure class="figure border-bottom">
-      <img class="figure-img img-fluid w-25 float-start me-3" src="images/team.jpg" alt="football team">
-      <article>${users[i].firstName} ${users[i].lastName}</article>
-      <figcaption class="figure-caption">${users[i].city} ${users[i].country}</figcaption>
-    </figure>
-    <hr>
-    
-`;
+    for (let i = 0; i < users.length; i++) {
+      var userDivString = `
+        <figure class="figure border-bottom">
+          <img class="figure-img img-fluid w-25 float-start me-3" src="images/team.jpg" alt="football team">
+          <article>${users[i].firstName} ${users[i].lastName}</article>
+          <figcaption class="figure-caption">${users[i].city} ${users[i].country}</figcaption>
+        </figure>
+        <hr>
+        
+    `;
 
-    const parser = new DOMParser();
-    var userDiv = parser.parseFromString(userDivString, "text/html").body
-      .firstChild;
-    mainElement.append(userDiv);
-  }
+      const parser = new DOMParser();
+      var userDiv = parser.parseFromString(userDivString, "text/html").body
+        .firstChild;
+      mainElement.append(userDiv);
+    }
+  });
 }
-
-//display users on html
